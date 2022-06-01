5fa23ef0c22e678f76f173557a2aee5fe98062b4
MORRIS MUTISYA SAMMY <morrehjayammsgk@gmail.com>
2022-06-01 12:10:56 +0300

    correction

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
-"""Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
+"""
+Rotate a 2D matrix by 90 degrees (clockwise).
+"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotate a 2D matrix 90 degrees clockwise."""
-    ziped = zip(*reversed(matrix))
-    for i, j in enumerate(ziped):
-        matrix[i] = list(j)
+    """
+    Rotate a 2D matrix by 90 degrees (clockwise).
+    """
+    n = len(matrix)
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
+
+            # left -> top
+            matrix[first][i] = matrix[last - offset][first]
+
+            # bottom -> left
+            matrix[last - offset][first] = matrix[last][last - offset]
+
+            # right -> bottom
+            matrix[last][last - offset] = matrix[i][last]
+
+            # top -> right
+            matrix[i][last] = top
+
+    return matrix
