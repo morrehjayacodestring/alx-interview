5e37546811dcbb18a6ddd643796ed765728a50ad
MORRIS MUTISYA SAMMY <morrehjayammsgk@gmail.com>
2022-06-01 12:24:38 +0300

    FINAL

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""returns the perimeter of the island described in grid"""
+"""
+Contains a method that returns the perimeter of the
+island described in grid
+"""
+
+
+def check_perimeter(grid, i, j):
+    """
+    check surroundings for water. If water body
+    on boundary, add 1 to perimeter otherwise add 0
+    """
+    mask = 1
+    top = grid[i - 1][j] ^ mask if i > 0 else 1
+    bottom = grid[i + 1][j] ^ mask if i < (len(grid) - 1) else 1
+    left = grid[i][j - 1] ^ mask if j > 0 else 1
+    right = grid[i][j + 1] ^ mask if j < (len(grid[i]) - 1) else 1
+    positions = top + bottom + right + left
+    return positions
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid"""
+    """
+    Calculates the perimeter of an island given a grid
+    Args:
+        grid (list): list of lists of integers
+    Returns:
+        int: perimeter of island
+    """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                perimeter += 4
-                # cells with 2 sides touching other cells on top and bottom
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                # cells with 2 sides touching other cells left and right
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                perimeter += check_perimeter(grid, i, j)
 
     return perimeter
