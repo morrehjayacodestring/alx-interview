1c4d783ddd7fda267a791e86834f89f45d41cbfe
MORRIS MUTISYA SAMMY <morrehjayammsgk@gmail.com>
2022-06-16 22:51:14 +0300

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
-"""
-This module contains the function that displays the
-stats from the standard input
-"""
-import re
+""" Log parsing module """
+
+import signal
 import sys
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-print_counter = 0
-size_summation = 0
 
+lp = []
+c = 0
+size = 0
+codes = {'200': 0,
+         '301': 0,
+         '400': 0,
+         '401': 0,
+         '403': 0,
+         '404': 0,
+         '405': 0,
+         '500': 0
+         }
 
-def print_logs():
-    """
-    Prints status codes to the logs
-    """
-    print("File size: {}".format(size_summation))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
-
-
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            std_line = line.replace("\n", "")
-            log_list = re.split('- | "|" | " " ', str(std_line))
-            try:
-                codes = log_list[-1].split(" ")
-                if int(codes[0]) not in status_codes.keys():
-                    continue
-                status_codes[int(codes[0])] += 1
-                print_counter += 1
-                size_summation += int(codes[1])
-                if print_counter % 10 == 0:
-                    print_logs()
-            except():
-                pass
-        print_logs()
-    except KeyboardInterrupt:
-        print_logs()
-        raise
+try:
+    for line in sys.stdin:
+        num = line.split(" ")[-2:]
+        if num and len(num) == 2:
+            if num[0] in codes.keys():
+                codes[num[0]] += 1
+            if num[1][:-1].isdigit():
+                size += int(num[1][:-1])
+            c += 1
+        if c % 10 == 0:
+            lp = list(codes.values())
+            print("File size: {}".format(size))
+            for x, y in codes.items():
+                if y:
+                    print('{}: {}'.format(x, y))
+finally:
+    if size:
+        print("File size: {}".format(size))
+        for x, y in codes.items():
+            if y:
+                print('{}: {}'.format(x, y))
